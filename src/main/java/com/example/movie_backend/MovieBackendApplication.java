67d0167d90eccb6381a506fc028c8be2310f7fb6
hanhathuy2612 <withuwe021@gmail.com>
2022-12-11 12:01:41 +0700

    update

--- src/main/java/com/example/movie_backend/MovieBackendApplication.java
+++ src/main/java/com/example/movie_backend/MovieBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MovieBackendApplication.class, args);
     }
-
 }
