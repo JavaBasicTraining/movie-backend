930fcf214b9d95b4c8dbaab18cf88a495b553e6a
hanhathuy2612 <withuwe021@gmail.com>
2022-12-11 22:12:16 +0700

    edit apptication config

--- src/main/java/com/example/movie_backend/MovieBackendApplication.java
+++ src/main/java/com/example/movie_backend/MovieBackendApplication.java
@@ -1,9 +1,11 @@
 package com.example.movie_backend;
 
+import com.example.boot.configuration.annotation.EnableAutoConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfig
 public class MovieBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(MovieBackendApplication.class, args);
--- src/main/java/com/example/movie_backend/config/OpenApiConfig.java
+++ src/main/java/com/example/movie_backend/config/OpenApiConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
+import org.springframework.context.annotation.Configuration;
 
 @SecuritySchemes(value = {
         @SecurityScheme(
