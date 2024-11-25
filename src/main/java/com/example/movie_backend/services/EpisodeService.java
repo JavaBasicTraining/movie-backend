b1505d11a81d02899aa34d29fe05684a32a44340
Thai <tihacro1202@gmail.com>
2024-11-25 14:22:52 +0700

    update-query-comment

--- src/main/java/com/example/movie_backend/services/EpisodeService.java
+++ src/main/java/com/example/movie_backend/services/EpisodeService.java
@@ -18,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
