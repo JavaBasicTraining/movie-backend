71cd857fae173df1c5cdee4f67e6ff3ef2b153c6
Thai <tihacro1202@gmail.com>
2024-12-13 16:30:43 +0700

    add-script-time-comment

--- src/main/resources/db/2.alter_commnet_add_fk.sql
+++ src/main/resources/db/2.alter_commnet_add_fk.sql
@@ -2,3 +2,6 @@ DROP TABLE IF EXISTS `movie_comment`;
 ALTER TABLE `comment`
     ADD COLUMN `movie_id` bigint DEFAULT NULL,
     ADD CONSTRAINT `FK_comment_movie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`);
+
+ALTER TABLE movie_website.comment
+ADD COLUMN time_comment DATETIME;
