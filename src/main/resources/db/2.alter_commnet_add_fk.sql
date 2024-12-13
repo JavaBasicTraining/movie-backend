64932270a292a0faf6453d2ac64711e575e3ff85
Thai <tihacro1202@gmail.com>
2024-12-13 16:50:09 +0700

    add-parrent-commentId

--- src/main/resources/db/2.alter_commnet_add_fk.sql
+++ src/main/resources/db/2.alter_commnet_add_fk.sql
@@ -5,3 +5,8 @@ ALTER TABLE `comment`
 
 ALTER TABLE comment
 ADD COLUMN time_comment DATETIME;
+
+
+ALTER TABLE `comment`
+ADD COLUMN `parent_comment_id` BIGINT DEFAULT NULL,
+ADD CONSTRAINT `FK_comment_parent` FOREIGN KEY (`parent_comment_id`) REFERENCES `comment`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
