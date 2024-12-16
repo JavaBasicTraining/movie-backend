9e02cc0156c092f6b9f4ef67776de0811bb344af
huyhanhat <withuwe021@gmail.com>
2024-12-16 13:03:16 +0700

    fix: get jwt from token

--- src/main/java/com/example/movie_backend/controller/impl/AuthenticationController.java
+++ src/main/java/com/example/movie_backend/controller/impl/AuthenticationController.java
@@ -3,7 +3,7 @@ package com.example.movie_backend.controller.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     @GetMapping
-    public void authenticate(@AuthenticationPrincipal JwtAuthenticationToken authentication) {
-        log.info("This func call to check if token is available or not: {}", authentication.getToken().getSubject());
+    public void authenticate(@AuthenticationPrincipal Jwt jwt) {
+        log.info("This func call to check if token is available or not: {}", jwt.getSubject());
     }
 }
