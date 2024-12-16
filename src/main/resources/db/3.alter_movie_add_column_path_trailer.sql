a20ab69615a8c1c220c5f5e2f73a035e94f3bf61
Thai <tihacro1202@gmail.com>
2024-12-16 11:48:24 +0700

    delete-movie-website-repo

--- src/main/resources/db/3.alter_movie_add_column_path_trailer.sql
+++ src/main/resources/db/3.alter_movie_add_column_path_trailer.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `movie_website`.`movie`
+ALTER TABLE `movie`
 ADD COLUMN `path` VARCHAR(255) UNIQUE;
 
-ALTER TABLE `movie_website`.`movie`
+ALTER TABLE `movie`
 ADD COLUMN `trailer_url` VARCHAR(255) NULL ;
